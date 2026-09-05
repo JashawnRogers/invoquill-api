6cc4d95395e1900704b861eeae6b9c2b52405ebf
JashawnRogers <rogersjashawn20@gmail.com>
2026-09-05 10:57:14 -0700

    db(users table): Created database table for users

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    user_status VARCHAR(30) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ
+);
